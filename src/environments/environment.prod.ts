5e27dc7ccd66ed99b82f79ea7b61d43b32821cae
Chizhevsky (Aliaksei Chyzheuski) <alexvinsent96@yandex.ru>
2018-05-09 18:29:13 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDub2skpWcmg8YxfgQ0EdyFVeiXogI7tk8",
-    authDomain: "angularfs-3b1fa.firebaseapp.com",
-    databaseURL: "https://angularfs-3b1fa.firebaseio.com",
-    projectId: "angularfs-3b1fa",
-    storageBucket: "angularfs-3b1fa.appspot.com",
-    messagingSenderId: "515858685952"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
